e27d8d04a6ca126564598984223f7128031d987f
bondarika <bondarika1@gmail.com>
2024-05-21 13:17:24 +0300

    dockerization

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:false,
-    images:{
-        unoptimized:true,
-        domains:['media.graphassets.com','lh3.googleusercontent.com']
-    }
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+    domains: ["media.graphassets.com", "lh3.googleusercontent.com"],
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
